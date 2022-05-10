0630d1d84b9ae10a84f0b3fcc0eede8dcc04a101
Anderson Mauri <maurianderson@outlook.com>
2022-05-10 14:30:26 -0300

    Radio Button

---
+++ src/components/RadioButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Radio = () => {
+    return (
+        <View>
+            <Text>Radio Button</Text>
+
+        </View>
+    )
+}
